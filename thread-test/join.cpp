d9e9e00614058e9526931c88af6321c8ef74703e
yan <yzc53@icloud.com>
2023-05-06 14:30:30 +0800

    5.6

---
+++ thread-test/join.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <thread>
+using namespace std;
+void thread_1()
+{
+  while(1)
+  {
+    cout<<"子线程1"<<endl;
+  }
+}
+void thread_2()
+{
+  while(1)
+  {
+    cout<<"子线程2"<<endl;
+  }
+}
+int main()
+{
+    thread first ( thread_1); // 开启线程，调用：thread_1()
+    thread second (thread_2); // 开启线程，调用：thread_2(100)
+
+    first.join(); // pauses until first finishes 这个操作完了之后才能destroyed
+    second.join(); // pauses until second finishes//join完了之后，才能往下执行。
+    while(1)
+    {
+      std::cout << "主线程\n";
+    }
+    return 0;
+}
